abf37fa0de7de65d049b857084a5d3cf3d394a4b
dodiwkwk <arifaldo0@gmail.com>
2021-02-12 15:59:44 +0700

    first commit, add tailwindcss and axios

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="m-auto flex justify-center mt-4 max-w-5xl">
+		<div class="flex flex-col justify-center">
+	        <img width="100" src="./assets/logo.png" />
+			<h3 class="text-center text-gray-800">Vue Ongkir Api</h3>
+		</div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+};
+</script>
